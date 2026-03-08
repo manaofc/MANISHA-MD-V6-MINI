60728c7534704a79f4669bfe78722d82b0b418fb
manaofc <manisha1718manisha@gmail.com>
2026-03-08 13:38:44 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -284,6 +284,50 @@ function setupCommandHandlers(socket, number, userConfig) {
 
     const cos = '```';
 
+
+    async function updateCMDStore(MsgID, CmdID) {
+  try {
+    let olds = readJSON("Non-Btn", "data.json", []);
+    olds.push({ [MsgID]: CmdID });
+    writeJSON("Non-Btn", "data.json", olds);
+    return true;
+  } catch (e) {
+    console.log(e);
+    return false;
+  }
+}
+
+async function isbtnID(MsgID) {
+  try {
+    let olds = readJSON("Non-Btn", "data.json", []);
+    return olds.some((item) => item[MsgID]);
+  } catch {
+    return false;
+  }
+}
+
+async function getCMDStore(MsgID) {
+  try {
+    let olds = readJSON("Non-Btn", "data.json", []);
+    for (const item of olds) {
+      if (item[MsgID]) {
+        return item[MsgID];
+      }
+    }
+    return null;
+  } catch (e) {
+    console.log(e);
+    return null;
+  }
+}
+
+function getCmdForCmdId(CMD_ID_MAP, cmdId) {
+  const result = CMD_ID_MAP.find((entry) => entry.cmdId === cmdId);
+  return result ? result.cmd : null;
+}
+
+
+
     // ---------------- BUTTON/REPLY HANDLERS -----------------
       conn.buttonMessage = async (jid, msgData, quotemek) => {
         if (!defaultConfig.NON_BUTTON) {
