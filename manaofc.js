bdb2d90d17e7646c2942f9db62b7fc52b17c199f
manaofc <manisha1718manisha@gmail.com>
2026-03-08 20:21:54 +0530

    Update manaofc.js

--- manaofc.js
+++ manaofc.js
@@ -29,24 +29,7 @@ const {
   delay
 } = require("baileys");
 
-
-// function.js
-const fetchJson = async (url, options) => {
-    try {
-        options ? options : {}
-        const res = await axios({
-            method: 'GET',
-            url: url,
-            headers: {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36'
-            },
-            ...options
-        })
-        return res.data
-    } catch (err) {
-        return err
-    }
-}
+const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson, getsize, formatBytes, fetchBuffer, formatSize, getFile } = require("./lib/functions");
 
 // Default config structure
 const defaultConfig = {
