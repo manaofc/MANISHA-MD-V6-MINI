6644c26ae4c5ae1539321147064082db57cc78e5
manaofc <manisha1718manisha@gmail.com>
2026-03-09 20:10:37 +0530

    Update manaofc.js

--- manaofc.js
+++ manaofc.js
@@ -1630,7 +1630,77 @@ async (socket, mek, m, { from, reply }) => {
 ////////////// SETTINGS COMMAND ////////////////
             ////////////////////
 
+cmd(
+  {
+    pattern: "settings",
+    react: "⚙️",
+    alias: ["setting", "botsetting"],
+    desc: "bot settings change",
+    category: "owner",
+    use: ".settings - *Bot Settings*",
+    filename: __filename,
+  },
+  async (socket, mek, m, { from, prefix, q, reply }) => {
+    try {
+      const sections = [
+        {
+          title: "💫 PREFIX",
+          rows: [
+            { title: "PREFIX .", rowId: prefix + "set PREFIX ." },
+            { title: "PREFIX /", rowId: prefix + "set PREFIX /" },
+            { title: "PREFIX ?", rowId: prefix + "set PREFIX ?" },
+            { title: "PREFIX !", rowId: prefix + "set PREFIX !" },
+            { title: "PREFIX #", rowId: prefix + "set PREFIX #" },
+            { title: "PREFIX &", rowId: prefix + "set PREFIX &" },
+          ],
+        },
 
+        {
+          title: "👁️ AUTO VIEW STATUS",
+          rows: [
+            { title: "✅ Enable Auto View Status", rowId: prefix + "set AUTO_VIEW_STATUS true" },
+            { title: "❎ Disable Auto View Status", rowId: prefix + "set AUTO_VIEW_STATUS false" },
+          ],
+        },
+
+        {
+          title: "🛟 AUTO LIKE STATUS",
+          rows: [
+            { title: "✅ Enable Auto Like Status", rowId: prefix + "set AUTO_LIKE_STATUS true" },
+            { title: "❎ Disable Auto Like Status", rowId: prefix + "set AUTO_LIKE_STATUS false" },
+          ],
+        },
+
+        {
+          title: "📱 AUTO RECORDING",
+          rows: [
+            { title: "✅ Enable Auto Recording", rowId: prefix + "set AUTO_RECORDING true" },
+            { title: "❎ Disable Auto Recording", rowId: prefix + "set AUTO_RECORDING false" },
+          ],
+        },
+      ];
+
+      const desc = `⚙️ \`MANISHA-MD-V6 BOT SETTINGS\` ⚙️
+    
+> ◈ *ᴏᴡɴᴇʀ:* manaofc
+> ◈ *ᴠᴇʀꜱɪᴏɴ:* ᴠ.6
+`;
+
+      let listset = {
+        text: desc,
+        footer: '> _*Powered By Manaofc*_ ',
+        title: "",
+        buttonText: "*🔢 Reply below number*",
+        sections,
+      };
+
+      await socket.listMessage(from, listset, mek);
+    } catch (e) {
+      reply("*❌ Error !!*");
+      console.log(e);
+    }
+  }
+);
 
   
 cmd({
