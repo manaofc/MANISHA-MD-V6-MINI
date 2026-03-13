82373dbbb883c52196f97034f41a92dea51dda7a
manaofc <manisha1718manisha@gmail.com>
2026-03-13 12:21:39 +0530

    Update manaofc.js

--- manaofc.js
+++ manaofc.js
@@ -1742,6 +1742,37 @@ reply("❌ Config update failed!")
 });
 
 
+  cmd({
+    pattern: "owner",
+    desc: "Display owner contact information.",
+    react: "🌝",
+    use: ".owner",
+    category: "owner",
+    filename: __filename
+},
+async (conn, mek, m, { from, reply }) => {
+    try {
+        const vcard = 
+            'BEGIN:VCARD\n' +
+            'VERSION:3.0\n' +
+            'FN:MANAOFC\n' +
+            'ORG:MANAOFC\n' +
+            'TEL;type=CELL;type=VOICE;waid=94759934522:+94759934522\n' +
+            'EMAIL:manishasasmith27@gmail.com\n' +
+            'END:VCARD';
+
+        await conn.sendMessage(from, { 
+            contacts: { 
+                displayName: "manaofc", 
+                contacts: [{ vcard }] 
+            },  
+            quoted: mek 
+        });
+    } catch (e) {
+        console.error(e);
+        reply('⚠️ An error occurred while fetching owner information.');
+    }
+});
     /* ================== MESSAGE HANDLER ================== */
     socket.ev.on("messages.upsert", async ({ messages }) => {
         const mek = messages[0];
