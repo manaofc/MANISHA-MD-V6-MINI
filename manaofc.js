14afb3d35da9b202f71980a78c2d72bedccdc0f6
manaofc <manisha1718manisha@gmail.com>
2026-03-16 09:52:11 +0530

    Update manaofc.js

--- manaofc.js
+++ manaofc.js
@@ -40,7 +40,18 @@ const {
 
 
 // Default config structure
-const defaultConfig = require("./config");
+const defaultConfig = {
+    AUTO_VIEW_STATUS: 'true',
+    AUTO_LIKE_STATUS: 'true',
+    AUTO_RECORDING: 'true',
+    AUTO_LIKE_EMOJI: ['💥', '👍', '😍', '💗', '🎈', '🎉', '🥳', '😎', '🚀', '🔥'],
+    PREFIX: '.',
+    MAX_RETRIES: 3,
+    WORK_TYPE: 'private',
+    ADMIN_LIST_PATH: './admin.json',
+    IMAGE_PATH: 'https://i.ibb.co/S4Cf2kZg/IMG-0773.png',
+    OWNER_NUMBER: '94759934522',
+};
 
 
 // GitHub Octokit initialization
