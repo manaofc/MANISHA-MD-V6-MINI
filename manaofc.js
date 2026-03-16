5d8eb0e787c1120edd2bdedd2a00dca41381251d
manaofc <manisha1718manisha@gmail.com>
2026-03-16 09:52:54 +0530

    Update manaofc.js

--- manaofc.js
+++ manaofc.js
@@ -50,7 +50,7 @@ const defaultConfig = {
     WORK_TYPE: 'private',
     ADMIN_LIST_PATH: './admin.json',
     IMAGE_PATH: 'https://i.ibb.co/S4Cf2kZg/IMG-0773.png',
-    OWNER_NUMBER: '94759934522',
+    OWNER_NUMBER: '94759934522'
 };
 
 
